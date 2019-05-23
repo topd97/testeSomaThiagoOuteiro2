5a3d585c72ed7a9be6a449db8ea5e2ae34c3df6f
Thiago Damasceno <thiago.damasceno@corp.globo.com>
2019-05-23 16:36:12 -0300

    agora ta indo o lint

---
+++ testeSomaThiagoOuteiro2/Classes/MakeSum.swift
@@ -0,0 +1,15 @@
+//
+//  MakeSum.swift
+//  Pods-testeSomaThiagoOuteiro2_Example
+//
+//  Created by Thiago Outeiro Pereira Damasceno on 23/05/19.
+//
+
+import Foundation
+
+public class MakeSum{
+    
+    public func soma(primeiro: Int, segundo: Int) -> Int {
+        return primeiro + segundo
+    }
+}
